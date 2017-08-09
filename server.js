e29860f6be9c388540e7c72535e930428c79772e
Joseph <riverfish@gmail.com>
2017-08-10 00:55:00 +0100

    Change server.js and package.json

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
+// const express = require('express');
+// const path = require('path');
+// const port = process.env.PORT || 8080;
+// const app = express();
+//
+// app.use(express.static(__dirname));
+//
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'index.html'));
+// });
+//
+// app.listen(port);
+// console.log('Server started');
+'use strict';
+
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+
 const app = express();
 
-app.use(express.static(__dirname));
+// Setup view engine
+app.set('view engine', 'pug');
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.use(express.static(path.resolve(path.join(__dirname, '/dist'))));
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.listen(port);
-console.log('Server started');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
